604139d3c10093c6e3e704fa29dbb82ff29ef2e2
mneskarous <40720186+mneskarous@users.noreply.github.com>
2018-07-05 15:06:31 -0400

    Update app.js
    Version 1.2

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /* A list that holds all of the cards */
-
 var deckOfCards = [
   '<i class="fas fa-fish"></i>',
   '<i class="fas fa-fish"></i>',
@@ -19,13 +18,10 @@ var deckOfCards = [
   '<i class="fas fa-gem"></i>',
 ];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- */
+/* --Displaying the cards on the page-- */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
+/* Function that shuffles the list of cards using the provided "shuffle" method below.  Shuffle function from http://stackoverflow.com/a/2450976 */
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -35,43 +31,64 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+/* The shuffled deck of cards are assigned a variable */
 var shuffledDeckOfCards = shuffle(deckOfCards);
 
-/*  *   - loop through each card and create its HTML
-*/
 function createHtmlCard(shuffledDeckOfCards) {
+/*  Function that loops through each card and create its HTML */
   var htmlCard = '';
 
-  for (var i = shuffledDeckOfCards.length - 1; i >= 0; i--){
+  for (var i = shuffledDeckOfCards.length - 1; i >= 0; i--) {
     htmlCard += '<li class = "card">' + shuffledDeckOfCards[i] + '</li>';
-    }
-
-    return htmlCard;
+  }
+  return htmlCard;
 }
 
-/*   *   - add each card's HTML to the page
-*/
-
+/*  Adds each card's HTML to the page */
 $('.deck').html(createHtmlCard(shuffledDeckOfCards));
 
+/* --When cards are clicked-- */
 
- /* - display the card's symbol (put this functionality in another function that you call from this one) */
-$(".deck .card").on("click", function() {
-    $(this).toggleClass(".card show open");
+/* When a card is clicked, the card is opened and it's display is shown */
+$('.deck .card').on('click', function() {
+  $(this).addClass('.card show open');
 });
 
+/* When a card is clicked, it's added to a *list* of "open" cards */
+var openCards = [];
+$('.deck .card').on('click', function() {
+  $('.card show open').each(function() {
+    openCards.push($(this));
+  });
+});
+
+if (openCards.length === 2) {
+/* If the list of open cards already has another card, two cards are checked to see if they match */
+  if (openCards[0] === openCards[1]) {
+  /* If the cards do match, the cards are locked in the open position */
+    $('.card show open').addClass('.card match').removeClass('.card show open');
+  }
+  else {
+    /* If the cards do not match, the cards are removed from the list and the card's are put in the closed position */
+    openCards.length = 0;
+    $('.card show open').addClass('.deck .card').removeClass('.card show open');
+  }
+}
+
+/* Function that restarts the game when the Restart button on the Score Panel is clicked */
+$('.score-panel .restart').on('click', function() {
+  var shuffledDeckOfCards = shuffle(deckOfCards);
+  $('.deck').html(createHtmlCard(shuffledDeckOfCards));
+  $('.deck .card').on('click', function() {
+    $(this).addClass('.card show open');
+  });
+});
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
